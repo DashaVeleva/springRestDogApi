264d0bb6ce783c921ed00ee0523ec71b64509370
Darya <dashaveleva@yandex.ru>
2022-08-09 00:53:33 +0300

    update tests

--- src/main/java/com/dashaveleva/rest/controller/AppController.java
+++ src/main/java/com/dashaveleva/rest/controller/AppController.java
@@ -1,38 +1,55 @@
 package com.dashaveleva.rest.controller;
 
 import com.dashaveleva.rest.entity.Dog;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/dogs")
 public class AppController {
 
-    public static Map<Integer, Dog> allDogs = new HashMap();
+    public static Map<Integer, Dog> allDogs = new ConcurrentHashMap<>();
+    private static AtomicInteger idGenerator = new AtomicInteger();
 
-    @PostMapping("/dogs/{id}")
-    public void createDog(@PathVariable int id, @RequestBody Dog dog) {
+    @PostMapping
+    public Dog createDog(@RequestBody Dog dog) {
+        int id = idGenerator.incrementAndGet();
+        dog.setId(id);
         allDogs.put(id, dog);
-    }
-
-    @GetMapping("/dogs/{id}")
-    public Dog showADog(@PathVariable int id) {
-        Dog dog = allDogs.get(id);
         return dog;
     }
 
-    @PutMapping("/dogs/{id}")
-    public void updateDog(@PathVariable int id, @RequestBody Dog dog) {
-        allDogs.put(id, dog);
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getDog(@PathVariable int id) {
+        Dog dog = allDogs.get(id);
+        if(dog == null)
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return ResponseEntity.ok(dog);
     }
 
-    @DeleteMapping("/dogs/{id}")
-    public void deleteDog(@PathVariable int id) {
+    @GetMapping
+    public ResponseEntity<?> getAllDogs() {
+
+        if(allDogs.isEmpty())
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return ResponseEntity.ok(allDogs);
+    }
+
+    @PutMapping("/{id}")
+    public Dog updateDog(@PathVariable int id, @RequestBody Dog dog) {
+        allDogs.put(id, dog);
+        return dog;
+    }
+
+    @DeleteMapping("/{id}")
+    public Dog deleteDog(@PathVariable int id, @RequestBody Dog dog) {
         allDogs.remove(id);
+        return dog;
     }
 
 
--- src/main/java/com/dashaveleva/rest/entity/Dog.java
+++ src/main/java/com/dashaveleva/rest/entity/Dog.java
@@ -5,6 +5,7 @@ import org.springframework.format.annotation.DateTimeFormat;
 import java.time.LocalDate;
 
 public class Dog {
+    private int id;
     private String name; // 1-100 symbols
     private LocalDate dateOfBirth; // must be before NOW - localdatetime, can be null
     private int height;
@@ -15,12 +16,26 @@ public class Dog {
 
     }
 
+    public Dog(int id, String name, LocalDate dateOfBirth, int height, int weight) {
+        this.id = id;
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+        this.height = height;
+        this.weight = weight;
+    }
+
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public int getId() {
+        return id;
     }
 
     public LocalDate getDateOfBirth() {
